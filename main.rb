3b7773a452bfdf7d30ade4c60b0f668d3c7bbbef
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-07 10:01:50 -0400

    Add BeerKeg dependancy class for BarSystem

--- main.rb
+++ main.rb
@@ -1,35 +1,27 @@
 class Bar
 
-    attr_accessor :bar_tender
-    attr_reader :name
-    attr_reader :menu
-    attr_reader :bar_system
+    attr_accessor :bar_tender, :atmosphere
+    attr_reader :name, :menu, :bar_system
     
     def initialize(name, bar_tender)
         @name = name
         @bar_tender = bar_tender
-        @bar_system = BarSystem.new(["Le Chouffe", "Stella", "Blue Moon", "Canadian"])
+        @bar_system = BarSystem.new()
         @menu = {fries: 5, nachos: 12, wings: 12, beer: 5, whiskey: 8, cocktail: 8}
+        @atmosphere = {music: false, television: false}
     end
-
-=begin
-SHOULD THIS BE IN BAR?  SHOUD BarTender maybe set_atmosphere, and bar should store state of music and tv as bools?
-    def set_atmosphere(music, television)
-        if music:
-            
-        end
-
-        if television:
-
-    end
-=end
 end
 
 class BarSystem
-    attr_reader :on_tap
 
-    def initialize(on_tap)
-        @on_tap = on_tap
+    def initialize
+        @on_tap = []
+    end
+
+    def on_tap
+        for keg in @on_tap
+            puts keg
+        end
     end
 
     def on_tap=(beer)
@@ -45,21 +37,34 @@ class BarSystem
     end
 end
 
+class BeerKeg
+    attr_reader :keg_level, :beer_brand
+
+    def initialize(beer_brand)
+        @beer_brand = beer_brand
+        @keg_level = keg_level
+    end
+
+    def to_s()
+        "#{@beer_brand}"
+    end
+
+end
 
 ruby_bar = Bar.new("Ruby", "Viktor")
 
 puts ruby_bar.menu
 puts ruby_bar.bar_tender
 
-ruby_bar.bar_tender=("Hilary")                  # Shift change, Hilary is bar tender now
+ruby_bar.bar_tender=("Hilary")                                            # Shift change, Hilary is bar tender now
 
 puts ruby_bar.bar_tender
-puts ruby_bar.bar_system.on_tap
 
-ruby_bar.bar_system.pour_beer("Any")            # Pour a beer - we don't check whether beer exists yet or not.
+ruby_bar.bar_system.pour_beer("Any")                                      # Pour a beer - we don't check whether beer exists yet or not.
 
-ruby_bar.bar_system.on_tap=("Labatts")          # Add Labatts to on_tap
-ruby_bar.bar_system.on_tap=("Stella")           # Remove Stells from on_tap
-puts ruby_bar.bar_system.on_tap
+ruby_bar.bar_system.on_tap=(BeerKeg.new("Labatts"))                       # Add Labatts to on_tap
+ruby_bar.bar_system.on_tap=(BeerKeg.new("Stella"))                        # Add Stella to on_tap
+ruby_bar.bar_system.on_tap=(BeerKeg.new("Le Chouffe"))                    # Add Le Chouffe to on_tap
+ruby_bar.bar_system.on_tap=(BeerKeg.new("Labatts"))                       # Add Labatts to on_tap
 
-
+ruby_bar.bar_system.on_tap

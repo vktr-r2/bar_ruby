562b2f18354cee74685916598ef1dcc417e74c49
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-10 23:46:59 -0400

    Classify methods as private and protected, implement recite_menu

--- main.rb
+++ main.rb
@@ -5,36 +5,33 @@ class Bar
     def initialize(name, bar_tender)
         @name = name
         @bar_system = BarSystem.new()
-        @menu = {beer: 5, whiskey: 8, cocktail: 8, wine: 8}
+        @menu = {beer: 5, whiskey: 8, cocktails: 8, wine: 8}
         @atmosphere = {music: false, television: false}
-        @bar_tender = Bartender.new(bar_tender, @bar_system, @menu)              # Bartender should know what the bar serves and be able to serve it.
+        @bar_tender = Bartender.new(bar_tender, bar_system, menu)              # Bartender should know what the bar serves and be able to serve it.
     end
 end
 
 class BarSystem
-    attr_reader = :on_tap
+    attr_reader :on_tap
 
     def initialize
         @on_tap = []
     end
 
-    def on_tap
-        return @on_tap
-    end
-
+    protected
 
     def on_tap=(beer)
-        for keg in @on_tap
+        for keg in on_tap
             if beer == keg.beer_brand
-                @on_tap.delete(beer)
+                on_tap.delete(beer)
                 return
             end
         end
-        @on_tap << BeerKeg.new(beer)
+        on_tap << BeerKeg.new(beer)
     end
 
     def pour_beer(beer)
-        keg = @on_tap.find { |keg| keg.beer_brand == beer }
+        keg = on_tap.find { |keg| keg.beer_brand == beer }
         if keg.keg_level > 0
             keg.keg_level=(keg.keg_level - 1)
             return "*Glass of #{beer}*"
@@ -61,12 +58,12 @@ class Bartender
     end
     
     def to_s
-        "#{@name}"
+        "#{name}"
     end
     
     def chat
-        random_key = @chat_topics.keys.sample
-        puts @chat_topics[random_key]
+        random_key = chat_topics.keys.sample
+        puts chat_topics[random_key]
     end
     
     def order(*args)
@@ -83,13 +80,24 @@ class Bartender
     end
     
     def clean
-        puts "*#{@name} tidies up*"
+        puts "*#{name} tidies up*"
     end
-    
-    private
+
+    def change_tap(beer)
+        bar_system.send(:on_tap=, beer)
+    end
+
+    def recite_menu
+        menu_items = menu.keys
+        puts "We serve:"
+        puts menu_items
+        puts "For beer we have:"
+        puts bar_system.on_tap
+    end
+
 
     def check_menu(order_item)
-        return @bar_system.on_tap.find { |keg| keg.beer_brand == order_item } || @menu.find { |menu_item, price| menu_item == order_item }
+        return bar_system.on_tap.find { |keg| keg.beer_brand == order_item } || menu.find { |menu_item, price| menu_item == order_item }
     end
     
     def get_drink(drink)
@@ -97,7 +105,7 @@ class Bartender
     end
     
     def get_beer(beer)
-        return bar_system.pour_beer(beer)
+        bar_system.send(:pour_beer, beer)
     end
 end
 
@@ -111,43 +119,10 @@ class BeerKeg
     end
 
     def to_s()
-        "#{@beer_brand}"
+        "#{beer_brand}"
     end
 
 end
-
-
-=begin
-ruby_bar = Bar.new("Ruby", "Viktor")
-
-viktor = ruby_bar.bar_tender
-
-puts ruby_bar.menu
-puts ruby_bar.bar_tender
-
-hilary = Bartender.new("Hilary")
-ruby_bar.bar_tender=(hilary)                                                # Shift change, Hilary is bar tender now
-
-puts ruby_bar.bar_tender
-
-ruby_bar.bar_system.on_tap=("Labatts")                                      # Add Labatts to on_tap
-ruby_bar.bar_system.on_tap=("Stella")                                       # Add Stella to on_tap
-ruby_bar.bar_system.on_tap=("Le Chouffe")                                   # Add Le Chouffe to on_tap
-ruby_bar.bar_system.on_tap=("Labatts")                                      # Remove Labatts to on_tap
-ruby_bar.bar_system.on_tap
-
-ruby_bar.bar_system.pour_beer("Any")                                        # Pour a beer - we don't check whether beer exists yet or not.
-
-puts hilary
-puts hilary.class
-
-hilary.clean                                                                # Bartender can clean
-hilary.chat                                                                 # Bartender can chat
-hilary.make_order("pizza")                                                  # Bartender can make order - we still don't check whether order eixsts yet or not
-
-ruby_bar.bar_tender=(viktor)                                                # Viktor is back on shift now
-puts ruby_bar.bar_tender
-=end
 
 =begin
 # IDEA: Bartender should ask customer for right amount of money before making order
@@ -167,9 +142,15 @@ ruby_bar.bar_tender=(hilary)
 puts ruby_bar.bar_tender
 
 
-ruby_bar.bar_system.on_tap=("Labatts")                                      # Add Labatts to on_tap
-ruby_bar.bar_system.on_tap=("Stella")                                       # Add Stella to on_tap
-ruby_bar.bar_system.on_tap=("Le Chouffe")                                   # Add Le Chouffe to on_tap
+hilary.change_tap("Labatts")                                      # Add Labatts to on_tap
+hilary.change_tap("Stella")                                       # Add Stella to on_tap
+hilary.change_tap("Le Chouffe")                                   # Add Le Chouffe to on_tap
+hilary.change_tap("Labatts")                                      # Remove Labatts to on_tap
+hilary.change_tap("Canadian")                                     # Add Canadian
 
+puts hilary.bar_system.on_tap()
+
+hilary.recite_menu()
+hilary.get_beer("Stella")
 
 hilary.order("Stella", "fries", "pizza")

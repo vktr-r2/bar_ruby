ebc088126d0ec095a7dd8eb2ca8ddc5c9a08a451
Viktor R <122321007+vktr-r2@users.noreply.github.com>
2024-09-11 22:19:43 -0400

    Add Bartender class tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,13 +1,13 @@
 # spec/bar_spec.rb
-require 'rspec'
-require_relative '../main'
+require "rspec"
+require_relative "../main"
 
 RSpec.describe Bar do
   let(:bar) { Bar.new("Ruby Bar", "Viktor") }
   let(:bartender) { bar.bar_tender }
 
-  describe '#initialize' do
-    it 'creates a bar with a name, a menu, atmosphere, and a bartender' do
+  describe "#initialize" do
+    it "creates a bar with a name, a menu, atmosphere, and a bartender" do
       expect(bar.name).to eq("Ruby Bar")
       expect(bar.bar_tender.name).to eq("Viktor")
       expect(bar.menu).to include(:beer, :whiskey, :cocktail, :wine)
@@ -15,8 +15,8 @@ RSpec.describe Bar do
     end
   end
 
-  describe '#to_s' do
-    it 'returns the name of the bar' do
+  describe "#to_s" do
+    it "returns the name of the bar" do
       expect(bar.to_s).to eq("Ruby Bar")
     end
   end
@@ -25,22 +25,22 @@ end
 RSpec.describe BarSystem do
   let(:bar_system) { BarSystem.new }
 
-  describe '#initialize' do
-    it 'initializes with an empty on_tap array' do
+  describe "#initialize" do
+    it "initializes with an empty on_tap array" do
       expect(bar_system.on_tap).to eq([])
     end
   end
 
-  describe '#update_beers_on_tap' do
-    it 'adds a new beer to the on_tap array' do
+  describe "#update_beers_on_tap" do
+    it "adds a new beer to the on_tap array" do
       bartender = Bartender.new("Viktor", bar_system, {})
       bartender.update_beers_on_tap("Stella")
       expect(bar_system.on_tap.map(&:beer_brand)).to include("Stella")
     end
   end
 
-  describe '#pour_beer' do
-    it 'pours a beer and reduces the keg level' do
+  describe "#pour_beer" do
+    it "pours a beer and reduces the keg level" do
       bartender = Bartender.new("Viktor", bar_system, {})
       bartender.update_beers_on_tap("Stella")
       beer = bartender.send(:get_beer, "Stella")
@@ -50,13 +50,60 @@ RSpec.describe BarSystem do
       expect(keg.keg_level).to eq(99)
     end
 
-    it 'returns an empty keg message if beer is out' do
+    it "returns an empty keg message if beer is out" do
       bartender = Bartender.new("Viktor", bar_system, {})
       bartender.update_beers_on_tap("Stella")
       keg = bar_system.on_tap.find { |k| k.beer_brand == "Stella" }
       keg.keg_level = 0
       result = bartender.send(:get_beer, "Stella")
       expect(result).to eq("*Stella keg is empty*")
+    end
+  end
+end
+
+RSpec.describe Bartender do
+  let(:bar) { Bar.new("Ruby Bar", "Viktor") }
+  let(:bartender) { bar.bar_tender }
+
+  describe "#initialize" do
+    it "initializes with a name, bar system, and menu" do
+      expect(bartender.name).to eq("Viktor")
+      expect(bartender.bar_system).to be_a(BarSystem)
+      expect(bartender.menu).to include(:beer, :whiskey, :cocktail, :wine)
+    end
+  end
+
+  describe "#chat" do
+    it "outputs a random chat topic" do
+      expect { bartender.chat }.to output(/How was your day?|The weather today is crazy, right?|Man, the local sports team stinks!/).to_stdout
+    end
+  end
+
+  describe "#make_order" do
+    before do
+      bartender.update_beers_on_tap("Stella")
+    end
+
+    it "makes an order for an available drink" do
+      expect { bartender.make_order("Stella") }.to output(/Here is your order:/).to_stdout
+    end
+
+    it "informs customer if item is not available" do
+      expect { bartender.make_order("Margarita") }.to output(/Sorry, we don't sell Margarita/).to_stdout
+    end
+  end
+
+  describe "#update_beers_on_tap" do
+    it "updates the beers on tap" do
+      bartender.update_beers_on_tap("Le Chouffe")
+      expect(bartender.bar_system.on_tap.map(&:beer_brand)).to include("Le Chouffe")
+    end
+  end
+
+  describe "#recite_menu" do
+    it "lists menu items and beers on tap" do
+      bartender.update_beers_on_tap("Stella")
+      expect { bartender.recite_menu }.to output(/We serve:\nbeer\nwhiskey\ncocktail\nwine\nFor beer we have:/).to_stdout
     end
   end
 end
